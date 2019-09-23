489827e7e222f50f8bea84a443a10f2f29b27ede
Aviral Garg <contact@aviralgarg.com>
2019-09-22 17:48:54 -0700

    greoup the numbers optimization

--- 1_Sorting/practice-review/groupTheNumbers.py
+++ 1_Sorting/practice-review/groupTheNumbers.py
@@ -1,25 +1,12 @@
-def getLo(arr, lo, hi):
-    while lo < hi and arr[lo] % 2 == 0:
-        lo += 1
-    return lo
-
-
-def getHi(arr, lo, hi):
-    while lo < hi and arr[hi] % 2 != 0:
-        hi -= 1
-    return hi
-
-
-def swap(arr, lo, hi):
-    arr[lo], arr[hi] = arr[hi], arr[lo]
-
-
 def groupTheNumbers(arr):
     lo, hi = 0, len(arr) - 1
     while lo < hi:
-        lo = getLo(arr, lo, hi)
-        hi = getHi(arr, lo, hi)
-        swap(arr, lo, hi)
+        while lo < hi and arr[lo] % 2 == 0:
+            lo += 1
+        while lo < hi and arr[hi] % 2 != 0:
+            hi -= 1
+        if lo < hi:
+            arr[lo], arr[hi] = arr[hi], arr[lo]
 
     return arr
 
