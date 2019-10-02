cf807008049cf3babd07ecfc4afab83096702f9e
Aviral Garg <contact@aviralgarg.com>
2019-10-02 16:56:56 -0700

    3__64_min_path_sum

---
+++ DynamicProgramming/study/leetCode/3__64_min_path_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minPathSum(self, dp: List[List[int]]) -> int:
+
+        m = len(dp)
+        n = len(dp[0])
+
+        for c in range(1, n):
+            dp[0][c] += dp[0][c-1]
+
+        for r in range(1, m):
+            dp[r][0] += dp[r-1][0]
+
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] += min(dp[r-1][c], dp[r][c-1])
+
+        return dp[-1][-1]
