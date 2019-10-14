fe10f47295a4c04dbe27357334c564b7510508a8
Aviral Garg <contact@aviralgarg.com>
2019-10-14 15:39:52 -0700

    i/j to r/c (row/column) refactor

--- 4_ DynamicProgramming/study/leetCode/5__62_unique_paths.py
+++ 4_ DynamicProgramming/study/leetCode/5__62_unique_paths.py
@@ -1,7 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1 for i in range(m)] for j in range(n)]
-        for i in range(1, n):
-            for j in range(1, m):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        dp = [[1 for _i in range(m)] for _j in range(n)]
+        for r in range(1, n):
+            for c in range(1, m):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
         return dp[-1][-1]
