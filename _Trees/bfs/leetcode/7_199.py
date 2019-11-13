275bff5b9a028e19b49b35ac4ed67bf0abd42947
Aviral Garg <contact@aviralgarg.com>
2019-11-12 20:04:32 -0800

    199 binary tree right side view

---
+++ _Trees/bfs/leetcode/7_199.py
@@ -0,0 +1,41 @@
+# A node structure
+from collections import deque
+
+
+class Node:
+    # A utility function to create a new node
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+    @staticmethod
+    def lvl_largest(root):
+        if root is None:
+            return []
+        q = deque([root])
+        result = []
+        while q:
+            tmp = deque()
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+                tmp.append(node.val)
+            result.append(tmp[-1])
+        return result
+
+
+# Driver Code
+if __name__ == '__main__':
+    t_root = Node(1)
+    t_root.left = Node(2)
+    t_root.right = Node(3)
+    t_root.left.left = Node(4)
+    t_root.left.right = Node(7)
+
+    print("lvl_largest")
+
+    print(t_root.lvl_largest(t_root))
