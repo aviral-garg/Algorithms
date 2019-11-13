83722680c1a101686daf602c092c277e01dd4f0b
Aviral Garg <contact@aviralgarg.com>
2019-11-12 19:56:56 -0800

    102 Binary tree level order traversal

---
+++ _Trees/bfs/leetcode/1_102.py
@@ -0,0 +1,42 @@
+# A node structure
+from collections import deque
+
+
+class Node:
+    # A utility function to create a new node
+    def __init__(self, key):
+        self.val = key
+        self.left = None
+        self.right = None
+
+    @staticmethod
+    def level_order(root):
+        if root is None:
+            return[[]]
+        q = deque([root])
+        result = []
+        while q:
+            num_nodes = len(q)
+            tmp = []
+            for _ in range(num_nodes):
+                node = q.popleft()
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+                tmp.append(node.val)
+            result.append(tmp)
+        return result
+
+
+# Driver Code
+if __name__ == '__main__':
+    t_root = Node(1)
+    t_root.left = Node(2)
+    t_root.right = Node(3)
+    t_root.left.left = Node(4)
+    t_root.left.right = Node(5)
+
+    print("level_order")
+
+    print(t_root.level_order(t_root))
