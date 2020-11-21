9eba568f5f4c8d92ef7baa453b631e562a5db776
Aviral Garg <12901566+aviral-garg@users.noreply.github.com>
2020-11-21 09:51:48 -0800

    Used enumerate instead of range
    1. Added leetcode question #
    1. Used enumerate instead of range

--- 6_Arrays_and_String_Manipulations/6_pivot_index.py
+++ 6_Arrays_and_String_Manipulations/6_pivot_index.py
@@ -1,13 +1,17 @@
+# 724
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        
         s = sum(nums)
         lsum = 0
         
-        for i in range(len(nums)):
-            rsum = s - lsum - nums[i]
+        for i, current in enumerate(nums):
+            rsum = s - (lsum + current)
+            
             if lsum == rsum:
                 return i
-            lsum += nums[i]
+            
+            lsum += current
             
         return -1
+
